0539da38c94c46001289e0aef8399916920d94c2
Gideon Juve <gideon@isi.edu>
2015-01-26 10:33:19 -0800

    Add archiving script to generate output files

---
+++ bin/archive.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+function usage () {
+    echo "Usage: $0 -stage N"
+}
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+while [ $# -ne 0 ]; do
+    case "$1" in
+        -stage)
+            STAGE=$1
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$STAGE" ]; then
+    echo "ERROR: Specify -stage"
+    usage
+    exit 1
+fi
+
+if ! [ -z "$PBS_O_WORKDIR" ]; then
+    cd $PBS_O_WORKDIR
+fi
+
+CASE=$(./xmlquery CASE -valonly -silent)
+RUNDIR=$(./xmlquery RUNDIR -valonly -silent)
+RUNDIR=$(cd $RUNDIR && pwd)
+
+tar -cz -f ${CASE}.stage${STAGE}.tar.gz -C $RUNDIR ${CASE}.*
+
